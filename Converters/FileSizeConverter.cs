4df310fb05e72e67b22dbff2a5a7e0dcbbab8c3a
madsies <madsiesss@gmail.com>
2026-05-15 15:58:34 +0100

    Converted Raw Byte Count to Higher Denominations, i.e. KB, MB, GB

---
+++ Converters/FileSizeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace FolderOrganiser.Converters
+{
+    internal class FileSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int extension)
+            {
+                if (extension < 1024)
+                {
+                    return $"{extension} B";
+                }
+                else if (extension < 1024 * 1024)
+                {
+                    return $"{extension / 1024.0:F2} KB";
+                }
+                else if (extension < 1024 * 1024 * 1024)
+                {
+                    return $"{extension / (1024.0 * 1024):F2} MB";
+                }
+                else
+                {
+                    return $"{extension / (1024.0 * 1024 * 1024):F2} GB";
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
